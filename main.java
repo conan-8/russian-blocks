197b090f22fe662eaf953254827647d7af044abf
conan-8 <yiconan885@gmail.com>
2025-04-16 10:33:54 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        int n = 5; 
+        int n = 10; 
         
         for (int i = n; i >= 1; i--) { 
             for (int j = 1; j <= i; j++) { 
