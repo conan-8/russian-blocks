0b5bd59c36da7e58e29e45b93556b80384b21bbc
beckett <91700492+beckettclarke@users.noreply.github.com>
2025-04-16 10:29:06 -0400

    🧪 Test commit

--- main.java
+++ main.java
@@ -8,7 +8,7 @@ public class Main {
             for (int j = 1; j <= i; j++) { 
                 System.out.print(j + " ");
             }
-            System.out.println(); 
+            System.out.println(""); 
         }
     }
 }
